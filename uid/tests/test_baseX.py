aec8dcc4eec38d12ed61ded80eb5c3a6229c580a
Misha Milovidov <m.alexeyvich@gmail.com>
2019-01-21 16:00:12 -0700

    Add unit tests

--- uid/tests/test_baseX.py
+++ uid/tests/test_baseX.py
@@ -11,5 +11,29 @@ class BaseXTester(TestCase):
     def test_base2(self):
         conv = BaseXConverter('01')
         i = 1234567890
-        self.assertEqual(conv.convert(i), '{:0b}'.format(i))    # test against python's builtin
+        self.assertEqual(conv.convert(i), '{:0b}'.format(i))
         self.assertEqual(i, conv.invert(conv.convert(i)))
+
+    def test_base8(self):
+        conv = BaseXConverter('01234567')
+        i = 1234567890
+        self.assertEqual(conv.convert(i), '{:0o}'.format(i))
+        self.assertEqual(i, conv.invert(conv.convert(i)))
+
+    def test_base16(self):
+        conv = BaseXConverter('0123456789ABCDEF')
+        i = 1234567890
+        self.assertEqual(conv.convert(i), '{:0x}'.format(i).upper())
+        self.assertEqual(i, conv.invert(conv.convert(i)))
+
+    # def test_base58(self):
+    #     conv = BaseXConverter('01')
+    #     i = 1234567890
+    #     self.assertEqual(conv.convert(i), '{:0b}'.format(i))
+    #     self.assertEqual(i, conv.invert(conv.convert(i)))
+    #
+    # def test_base64(self):
+    #     conv = BaseXConverter('01')
+    #     i = 1234567890
+    #     self.assertEqual(conv.convert(i), '{:0b}'.format(i))
+    #     self.assertEqual(i, conv.invert(conv.convert(i)))
