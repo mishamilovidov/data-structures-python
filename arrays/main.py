abdfa5c03ffee613fd860ccc6d3d8cfe43f8484f
Misha Milovidov <m.alexeyvich@gmail.com>
2019-02-07 22:51:03 -0700

    Set up main function to iterate over and execute commands

---
+++ arrays/main.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+import csv
+from array_api import Array
+
+CSV_FILE = 'data_example.csv'
+COMMANDS = {
+  'CREATE': { 'method': '', 'param_count': 0 },
+  'DEBUG': { 'method': 'debug_print', 'param_count': 0 },
+  'ADD': { 'method': 'add', 'param_count': 1 },
+  'SET': { 'method': 'set', 'param_count': 2 },
+  'GET': { 'method': 'get', 'param_count': 1 },
+  'DELETE': { 'method': 'delete', 'param_count': 1 },
+  'INSERT': { 'method': 'insert', 'param_count': 2 },
+  'SWAP': { 'method': 'swap', 'param_count': 2 },
+}
+
+def exec_command(command, arr, param_one, param_two):
+  method = COMMANDS[command]['method']
+  param_count = COMMANDS[command]['param_count']
+
+  if param_count == 2:
+    getattr(Array, method)(arr, param_one, param_two) 
+  elif param_count == 1:
+    getattr(Array, method)(arr, param_one) 
+  else:
+    getattr(Array, method)(arr)  
+
+def main():
+  with open(CSV_FILE) as file:
+    csv_reader = csv.reader(file, delimiter=',')
+    line = 0
+    
+    for row in csv_reader:
+      command = row[0]
+      param_one = row[1]
+      param_two = row[2]
+    
+      # print line number and command
+      print('{}:{},{},{}'.format(line, command, param_one, param_two))
+      
+      # call appropriate method based on command
+      if command == 'CREATE':
+        arr = Array()
+      else:
+        exec_command(command, arr, param_one, param_two)
+
+      line += 1
+  
+if __name__ == '__main__':
+  main()
+  
