cded25417ab35571b45a0c702e0cbc298b604fbd
Misha Milovidov <m.alexeyvich@gmail.com>
2019-02-11 11:39:42 -0700

    Fix output

--- arrays/array_api.py
+++ arrays/array_api.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-
 class Array(object):
   '''
   An array implementation that holds arbitrary objects.
@@ -8,9 +7,9 @@ class Array(object):
 
   def __init__(self, initial_size=10, chunk_size=5):
     '''Creates an array with an intial size.'''
-    print('>>>>>>>> {}'.format(initial_size))
     self.data = alloc(initial_size)
     self.size = 0
+    self.chunk_size = chunk_size
 
 
   def debug_print(self):
@@ -20,13 +19,14 @@ class Array(object):
 
   def _check_bounds(self, index):
     '''Ensures the index is within the bounds of the array: 0 <= index <= size.'''
-
+    return True if (int(index) >= 0 and int(index) <= self.size - 1) else False
 
   def _check_increase(self):
     '''
     Checks whether the array is full and needs to increase by chunk size
     in preparation for adding an item to the array.
     '''
+    return False if ((self.size + 1) <= len(self.data)) else True
 
 
   def _check_decrease(self):
@@ -35,31 +35,118 @@ class Array(object):
     If a decrease is warranted, it should be done by allocating a new array and copying the
     data into it (don't allocate multiple arrays if multiple chunks need decreasing).
     '''
-
+    remove_chunk = ((self.size - 1) % self.chunk_size) == 0
+    
+    return True if remove_chunk else False
+  
 
   def add(self, item):
-    '''Adds an item to the end of the array, allocating a larger array if necessary.'''
+    '''Adds an item to the end of the array, allocating a larger array if necessary.'''    
+    if self._check_increase():
+      size = len(self.data) + self.chunk_size
+      data = memcpy(alloc(size), self.data, len(self.data))
+    else:
+      size = len(self.data)
+      data = memcpy(alloc(size), self.data, len(self.data))
+    
+    for index, i in enumerate(data):
+      if i is None:
+        data[index] = item
+        break
+    
+    self.data = data 
+    self.size = self.size + 1
 
 
   def insert(self, index, item):
     '''Inserts an item at the given index, shifting remaining items right and allocating a larger array if necessary.'''
-
+    try:
+      if self._check_bounds(index):
+        if self._check_increase():
+          size = len(self.data) + self.chunk_size
+          data = memcpy(alloc(size), self.data, len(self.data))
+        else:
+          size = len(self.data)
+          data = memcpy(alloc(size), self.data, len(self.data))
+        
+        for idx, i in enumerate(data):
+          if i is None:
+            data[idx] = data[idx - 1]
+            break
+          
+        for i in range((len(data) - 1), int(index), -1) :
+          data[i] = data[i - 1]
+          
+        for idx, i in enumerate(data):
+          if i is None:
+            data[idx - 1] = None
+            break
+            
+        data[int(index)] = item
+        
+        self.data = data
+        self.size = self.size + 1
+      else:
+        raise ValueError
+    except ValueError:
+      print('Error: {} is not within the bounds of the current array.'.format(index))
 
   def set(self, index, item):
     '''Sets the given item at the given index.  Throws an exception if the index is not within the bounds of the array.'''
-
+    try:
+      if self._check_bounds(index):
+        self.data[int(index)] = item
+      else:
+        raise ValueError
+    except ValueError:
+      print('Error: {} is not within the bounds of the current array.'.format(index))
 
   def get(self, index):
     '''Retrieves the item at the given index.  Throws an exception if the index is not within the bounds of the array.'''
-
+    try:
+      if self._check_bounds(index):
+        print(self.data[int(index)])
+      else:
+        raise ValueError
+    except ValueError:
+      print('Error: {} is not within the bounds of the current array.'.format(index))
 
   def delete(self, index):
     '''Deletes the item at the given index, decreasing the allocated memory if needed.  Throws an exception if the index is not within the bounds of the array.'''
-
+    try:
+      if self._check_bounds(index):
+        if self._check_decrease():
+          data = alloc(len(self.data) - self.chunk_size)
+        else:
+          data = alloc(len(self.data))
+          
+        for i in range(int(index)):
+          data[i] = self.data[i]
+          
+        for i in range(int(index), (self.size - 1)):
+          data[i] = self.data[i+1]
+        
+        self.data = data
+        self.size = self.size - 1
+      else:
+        raise ValueError
+    except ValueError:
+      print('Error: {} is not within the bounds of the current array.'.format(index))
 
   def swap(self, index1, index2):
     '''Swaps the values at the given indices.'''
-
+    try:
+      if not self._check_bounds(index1):
+        raise ValueError(index1)    
+      elif not self._check_bounds(index2):
+        raise ValueError(index2)
+      else:
+        data = memcpy(alloc(len(self.data)), self.data, len(self.data))
+        
+        self.data[int(index1)] = data[int(index2)]
+        self.data[int(index2)] = data[int(index1)]
+    except ValueError as index:
+      print('Error: {} is not within the bounds of the current array.'.format(index))
 
 ###################################################
 ###   Utilities
@@ -78,3 +165,6 @@ def memcpy(dest, source, size):
   '''
   Copies items from one array to another.  This is similar to C's memcpy function.
   '''
+  for i in range(size):
+    dest[i] = source[i]
+  return dest
--- arrays/main.py
+++ arrays/main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+import sys
 import csv
 from array_api import Array
 
-CSV_FILE = 'data_example.csv'
+FILE_INPUT = 'data.csv'
+FILE_OUTPUT = 'output.txt'
 COMMANDS = {
   'CREATE': { 'method': '', 'param_count': 0 },
   'DEBUG': { 'method': 'debug_print', 'param_count': 0 },
@@ -26,26 +28,28 @@ def exec_command(command, arr, param_one, param_two):
     getattr(Array, method)(arr)  
 
 def main():
-  with open(CSV_FILE) as file:
-    csv_reader = csv.reader(file, delimiter=',')
-    line = 0
-    
-    for row in csv_reader:
-      command = row[0]
-      param_one = row[1]
-      param_two = row[2]
-    
-      # print line number and command
-      print('{}:{},{},{}'.format(line, command, param_one, param_two))
+  with open(FILE_INPUT) as input_file:
+    with open(FILE_OUTPUT, 'w') as output_file:
+      sys.stdout = output_file
+      csv_reader = csv.reader(input_file, delimiter=',')
+      line = 0
       
-      # call appropriate method based on command
-      if command == 'CREATE':
-        arr = Array()
-      else:
-        exec_command(command, arr, param_one, param_two)
-
-      line += 1
+      for row in csv_reader:
+        command = row[0]
+        param_one = row[1]
+        param_two = row[2]
+      
+        # print line number and command
+        print('{}:{},{},{}'.format(line, command, param_one, param_two))
+        
+        # call appropriate method based on command
+        if command == 'CREATE':
+          arr = Array()
+        else:
+          exec_command(command, arr, param_one, param_two)
   
+        line += 1
+    
 if __name__ == '__main__':
   main()
   
